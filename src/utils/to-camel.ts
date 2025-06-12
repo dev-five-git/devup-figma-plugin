747f245140d5e7c2e74ab13b55fa3b52889e84da
owjs3901 <owjs3901@naver.com>
2025-06-12 16:31:30 +0900

    Fix toCamel

--- src/utils/to-camel.ts
+++ src/utils/to-camel.ts
@@ -1,6 +1,11 @@
 export function toCamel(str: string): string {
   return str
     .split(/([A-Z][a-z]+|[-_ /])/)
+    .flatMap((e) => {
+      return /[^A-Z][A-Z]$/.test(e)
+        ? [e.slice(0, e.length - 1), e.slice(e.length - 1)]
+        : e
+    })
     .filter((e) => {
       if (!e || e === '-' || e === '_' || e === '/' || e === ' ') return false
       return true
