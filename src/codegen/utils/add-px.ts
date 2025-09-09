f22d34453d70855f28ace5d72f925d604ffd5f59
owjs3901 <owjs3901@gmail.com>
2025-09-09 20:21:14 +0900

    Add fallback to addPx

--- src/codegen/utils/add-px.ts
+++ src/codegen/utils/add-px.ts
@@ -1,9 +1,12 @@
-export function addPx(value: unknown) {
+export function addPx(
+  value: unknown,
+  fallback: string | undefined = undefined,
+) {
   if (typeof value !== 'number') return
   const fixed = value.toFixed(3)
   const str = fixed.endsWith('.000')
     ? String(Math.round(value))
     : fixed.replace(/\.?0+$/, '')
-  if (str === '0') return undefined
+  if (str === '0') return fallback
   return `${str}px`
 }
--- src/codegen/utils/four-value-shortcut.ts
+++ src/codegen/utils/four-value-shortcut.ts
@@ -7,12 +7,12 @@ export function fourValueShortcut(
   fourth: number,
 ): string {
   if (first === second && second === third && third === fourth)
-    return addPx(first)!
+    return addPx(first, '0')!
   if (first === third && second === fourth)
-    return `${addPx(first)} ${addPx(second)}`
+    return `${addPx(first, '0')} ${addPx(second, '0')}`
   if (first === third && second === fourth)
-    return `${addPx(first)} ${addPx(second)}`
+    return `${addPx(first, '0')} ${addPx(second, '0')}`
   if (second === fourth)
-    return `${addPx(first)} ${addPx(second)} ${addPx(third)}`
-  return `${addPx(first)} ${addPx(second)} ${addPx(third)} ${addPx(fourth)}`
+    return `${addPx(first, '0')} ${addPx(second, '0')} ${addPx(third, '0')}`
+  return `${addPx(first, '0')} ${addPx(second, '0')} ${addPx(third, '0')} ${addPx(fourth, '0')}`
 }
