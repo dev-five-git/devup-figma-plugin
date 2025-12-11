34cea1712be3adf6ef9ee7827d9d1a1402aa68a0
owjs3901 <owjs3901@gmail.com>
2025-12-11 20:26:39 +0900

    Split import logic

--- src/commands/devup/import-devup.ts
+++ src/commands/devup/import-devup.ts
@@ -1,10 +1,9 @@
 import { uploadFile } from '../../utils/upload-file'
-import type { Devup, DevupTypography } from './types'
+import { applyTypography } from './apply-typography'
+import { buildTargetStyleNames } from './build-target-style-names'
+import type { Devup } from './types'
 import { getDevupColorCollection } from './utils/get-devup-color-collection'
 import { uploadDevupXlsx } from './utils/upload-devup-xlsx'
-
-type TargetTypography = [target: string, typography: DevupTypography]
-const TYPO_PREFIX = ['mobile', '1', 'tablet', '3', 'desktop', '5'] as const
 
 export async function importDevup(input: 'json' | 'excel') {
   const devup = await loadDevup(input)
@@ -69,79 +68,5 @@ async function importTypography(devup: Devup) {
     for (const [target, typo] of targetStyleNames) {
       await applyTypography(target, typo, styles)
     }
-  }
-}
-
-function buildTargetStyleNames(
-  style: string,
-  value: DevupTypography | (DevupTypography | null)[],
-): TargetTypography[] {
-  const targets: TargetTypography[] = []
-  if (Array.isArray(value)) {
-    value.forEach((typo, idx) => {
-      if (!typo) return
-      const prefix = TYPO_PREFIX[idx] ?? `${idx}`
-      targets.push([`${prefix}/${style}`, typo])
-    })
-    return targets
-  }
-  targets.push([`mobile/${style}`, value])
-  return targets
-}
-
-async function applyTypography(
-  target: string,
-  typography: DevupTypography,
-  styles: TextStyle[],
-) {
-  const st = styles.find((s) => s.name === target) ?? figma.createTextStyle()
-  st.name = target
-  const fontFamily = {
-    family: typography.fontFamily ?? 'Inter',
-    style: typography.fontStyle === 'italic' ? 'Italic' : 'Regular',
-  }
-
-  try {
-    await figma.loadFontAsync(fontFamily)
-    st.fontName = fontFamily
-    if (typography.fontSize) st.fontSize = parseInt(typography.fontSize, 10)
-    if (typography.letterSpacing) {
-      st.letterSpacing = typography.letterSpacing.endsWith('em')
-        ? {
-            unit: 'PERCENT',
-            value: parseFloat(typography.letterSpacing),
-          }
-        : {
-            unit: 'PIXELS',
-            value: parseFloat(typography.letterSpacing) * 100,
-          }
-    }
-    if (typography.lineHeight) {
-      st.lineHeight =
-        typography.lineHeight === 'normal'
-          ? { unit: 'AUTO' }
-          : typeof typography.lineHeight === 'string'
-            ? {
-                unit: 'PIXELS',
-                value: parseInt(typography.lineHeight, 10),
-              }
-            : {
-                unit: 'PERCENT',
-                value: Math.round(typography.lineHeight / 10) / 10,
-              }
-    }
-    if (typography.textTransform) {
-      st.textCase = typography.textTransform.toUpperCase() as TextCase
-    }
-    if (typography.textDecoration) {
-      st.textDecoration =
-        typography.textDecoration.toUpperCase() as TextDecoration
-    }
-  } catch (error) {
-    console.error('Failed to create text style', error)
-    figma.notify(
-      `Failed to create text style (${target}, ${fontFamily.family} - ${fontFamily.style})`,
-      { error: true },
-    )
   }
 }
