162f786945872defd59aa8b591104c7e25890a36
owjs3901 <owjs3901@gmail.com>
2025-12-29 02:04:59 +0900

    Fix space

--- src/codegen/responsive/ResponsiveCodegen.ts
+++ src/codegen/responsive/ResponsiveCodegen.ts
@@ -768,10 +768,10 @@ export class ResponsiveCodegen {
               if (!childTree) continue
               const childCode = Codegen.renderTree(childTree, 0)
               const formattedChildCode = childCode.includes('\n')
-                ? `(\n${paddingLeftMultiline(childCode, depth)}\n  )`
+                ? `(\n${paddingLeftMultiline(childCode, 1)}\n)`
                 : childCode
               childrenCodes.push(
-                `  {${variantKey} === "${onlyVariant}" && ${formattedChildCode}}`,
+                `{${variantKey} === "${onlyVariant}" && ${formattedChildCode}}`,
               )
             } else {
               // Multiple (but not all) variants have this child
@@ -785,7 +785,7 @@ export class ResponsiveCodegen {
                 0,
               )
               const formattedChildCode = childCode.includes('\n')
-                ? `(\n${paddingLeftMultiline(childCode, depth)}\n  )`
+                ? `2(\n${paddingLeftMultiline(childCode, 1)}\n)`
                 : childCode
               childrenCodes.push(`{(${conditions}) && ${formattedChildCode}}`)
             }
